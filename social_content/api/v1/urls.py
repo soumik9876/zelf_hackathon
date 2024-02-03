7958f23316af8bd631a9a5dd965062e529bffd49
soumik9876 <soumik9876@gmail.com>
2024-02-03 17:01:52 +0600

    Implemented aggregation stats, content by platform and date apis

--- social_content/api/v1/urls.py
+++ social_content/api/v1/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from social_content.api.v1.views import ContentAuthorAPIView
+from social_content.api.v1.views import ContentAuthorAPIView, StatsDataAPIView, ContentByDate, ContentByPlatform
 
 urlpatterns = [
-    path('content-author/', ContentAuthorAPIView.as_view(), name='content-author')
+    path('content-author/', ContentAuthorAPIView.as_view(), name='content-author'),
+    path('stats/', StatsDataAPIView.as_view(), name='stats'),
+    path('content-by-date/', ContentByDate.as_view(), name='content-by-date'),
+    path('content-by-platform/', ContentByPlatform.as_view(), name='content-by-platform'),
 ]
--- social_content/api/v1/views.py
+++ social_content/api/v1/views.py
@@ -1,5 +1,7 @@
+from django.db.models import Avg, Max, Count
 from rest_framework.generics import ListAPIView, GenericAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from core.api.paginations import StandardResultsSetPagination
 from core.utils import get_logger
@@ -17,7 +19,7 @@ class ContentAuthorAPIView(ListAPIView):
     def get_queryset(self):
         """
             At first, we call the 3rd party content API according to request page number. We update or create the
-            content data in our own db, we also fetch author data conditionally and update them to DB. Finally we
+            content data in our own db, we also fetch author data conditionally and update them to DB. Finally, we
             serve the content and author data from our DB
         """
         page = self.request.query_params.get('page', 1)
@@ -41,3 +43,55 @@ class ContentAuthorAPIView(ListAPIView):
         #
         # serializer = self.get_serializer(queryset, many=True)
         # return Response(serializer.data)
+
+
+class StatsDataAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        aggregates = Content.objects.aggregate(
+            Avg('stats__likes'),
+            Avg('stats__views'),
+            Avg('stats__comments'),
+            Max('stats__likes'),
+            Max('stats__views'),
+            Max('stats__comments'),
+            Max('author__stats__followers')
+        )
+
+        aggregate_map = {
+            'avg_likes': 'stats__likes__avg',
+            'avg_views': 'stats__views__avg',
+            'avg_comments': 'stats__comments__avg',
+            'max_likes': 'stats__likes__max',
+            'max_views': 'stats__views__max',
+            'max_comments': 'stats__comments__max',
+            'most_followed': 'author__stats__followers__max'
+        }
+        response = {key: aggregates[value] for key, value in aggregate_map.items()}
+
+        most_popular_platform = Content.objects.values_list('origin_details__origin_platform') \
+            .annotate(cnt=Count('origin_details__origin_platform')).order_by('-cnt').first()
+        if most_popular_platform is not None:
+            response.update({
+                'most_popular_platform': {
+                    'name': most_popular_platform[0],
+                    'post_count': most_popular_platform[1]
+                }
+            })
+
+        return Response(response)
+
+
+class ContentByDate(APIView):
+    def get(self, request, *args, **kwargs):
+        content_by_date = Content.objects.values('originally_created_at').annotate(
+            cnt=Count('originally_created_at')).order_by('originally_created_at')
+
+        return Response(content_by_date)
+
+
+class ContentByPlatform(APIView):
+    def get(self, request, *args, **kwargs):
+        content_by_platform = Content.objects.values('origin_details__origin_platform').annotate(
+            cnt=Count('origin_details__origin_platform')).order_by('origin_details__origin_platform')
+
+        return Response(content_by_platform)
