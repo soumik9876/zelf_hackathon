cf6b01a06ff803776702621dd6439db3a9c982d2
soumik9876 <soumik9876@gmail.com>
2024-02-03 14:57:20 +0600

    Implemented custom content manager class

---
+++ social_content/managers/content_manager.py
@@ -0,0 +1,40 @@
+from django.db.models import Manager
+
+from social_content.models import Author
+
+
+class ContentManager(Manager):
+    def create_or_update_from_response(self, response):
+        contents = response.get('data')
+        for content in contents:
+            author = content.get('author')
+            author_obj = Author.objects.create_or_update_author_from_data(data=author)
+
+            content_data = {
+                'hack_uid': content.get('unique_id'),
+                'originally_created_at': content.get('creation_info').get('created_at'),
+                'context': content.get('context'),
+                'origin_details': content.get('origin_details'),
+                'media': content.get('media'),
+                'stats': self.get_stats(stats=content.get('stats')),
+                'author': author_obj.id
+            }
+            from social_content.api.v1.serializers import ContentSerializer
+            from social_content.models import Content
+            content_obj = Content.objects.filter(hack_uid=content_data.get('hack_uid')).first()
+            if content_obj is None:
+                serializer = ContentSerializer(data=content_data)
+                serializer.is_valid()
+                serializer.save()
+            else:
+                serializer = ContentSerializer(instance=content_obj, data=content_data, partial=True)
+                serializer.is_valid()
+                serializer.save()
+
+    def get_stats(self, stats):
+        digg_counts = stats.get('digg_counts')
+        return {
+            'likes': digg_counts.get('likes').get('count'),
+            'views': digg_counts.get('views').get('count'),
+            'comments': digg_counts.get('comments').get('count'),
+        }
